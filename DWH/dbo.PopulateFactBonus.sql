3289145944921c0baa342f4636f9bac25e8a14a3
Sergey <sergey.dvornikov@gmail.com>
2012-04-12 16:21:18 +0400

    Расчет по типа Субдилер + добавил дату начала для новых мотиваций

--- DWH/dbo.PopulateFactBonus.sql
+++ DWH/dbo.PopulateFactBonus.sql
@@ -161,6 +161,30 @@ where ji.project=10280
 	and ji.issuetype=39 
 	and ji.resolution=1 
 	and ji.issuestatus=6
+	and cg.CREATED>='2012-04-01'
+	and newvalue='6'
+group by ji.id,dimIssueType.uid,dimPerson.uid;
+
+-- Расчет бонуса для КЦ по запросам типа Субдилер(платежи)
+insert into dbo.factBonus (date_uid,person_uid,issuetype_uid,bonustype_uid,bonus,issueid)
+select
+	 ISNULL(MAX(dimDate.DateKey),-1) date_uid
+	,ISNULL(dimPerson.uid,-1) person_uid
+	,ISNULL(dimIssueType.uid,-1) issuetype_uid
+	,5 bonustype_uid
+	,10 bonus
+	,ji.ID issueid
+from jiraissue ji
+	join changegroup cg on cg.issueid=ji.id
+	join changeitem ci on ci.groupid=cg.id and field='status'
+	left outer join dimDate on dimDate.FullDate=DATEADD(dd, 0, DATEDIFF(dd, 0, cg.created))
+	left outer join dimPerson on dimPerson.ADname=ji.assignee
+	left outer join dimIssueType on dimIssueType.issuetype_id=ji.issuetype and dimIssueType.project_id=ji.PROJECT
+where ji.project=10280 
+	and ji.issuetype=24 
+	and ji.resolution=1 
+	and ji.issuestatus=6
+	and cg.CREATED>='2012-04-01'
 	and newvalue='6'
 group by ji.id,dimIssueType.uid,dimPerson.uid;
 
