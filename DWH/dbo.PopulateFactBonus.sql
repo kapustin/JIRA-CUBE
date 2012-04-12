8492963356cef0c15ff531c720b9fa47f81c1deb
Sergey <sergey.dvornikov@gmail.com>
2012-04-12 16:17:28 +0400

    Добавил расчет по запросу Платеж

--- DWH/dbo.PopulateFactBonus.sql
+++ DWH/dbo.PopulateFactBonus.sql
@@ -142,6 +142,28 @@ where ji.project=10311
 		or (oldvalue='10052' and newvalue='10052')) -- переписка в переписку
 group by ji.id,dimIssueType.uid,dimPerson.uid;
 
+-- Расчет бонуса для КЦ по запросам типа Платеж
+insert into dbo.factBonus (date_uid,person_uid,issuetype_uid,bonustype_uid,bonus,issueid)
+select
+	 ISNULL(MAX(dimDate.DateKey),-1) date_uid
+	,ISNULL(dimPerson.uid,-1) person_uid
+	,ISNULL(dimIssueType.uid,-1) issuetype_uid
+	,4 bonustype_uid
+	,10 bonus
+	,ji.ID issueid
+from jiraissue ji
+	join changegroup cg on cg.issueid=ji.id
+	join changeitem ci on ci.groupid=cg.id and field='status'
+	left outer join dimDate on dimDate.FullDate=DATEADD(dd, 0, DATEDIFF(dd, 0, cg.created))
+	left outer join dimPerson on dimPerson.ADname=ji.assignee
+	left outer join dimIssueType on dimIssueType.issuetype_id=ji.issuetype and dimIssueType.project_id=ji.PROJECT
+where ji.project=10280 
+	and ji.issuetype=39 
+	and ji.resolution=1 
+	and ji.issuestatus=6
+	and newvalue='6'
+group by ji.id,dimIssueType.uid,dimPerson.uid;
+
 SET NOCOUNT ON -- turn the annoying messages back on
 END
 
