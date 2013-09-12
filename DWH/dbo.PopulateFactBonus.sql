c54086fea303f6fe998f9cf3d659302bceb27161
Sergey <sergey.dvornikov@gmail.com>
2013-09-12 06:31:43 -0700

    Исполнитель и стоимость проекта собирается из сабтасков

--- DWH/dbo.PopulateFactBonus.sql
+++ DWH/dbo.PopulateFactBonus.sql
@@ -329,24 +329,31 @@ group by ji.id,dimIssueType.uid,dimPerson.uid;
 insert into dbo.factBonus (date_uid,person_uid,issuetype_uid,bonustype_uid,bonus,issueid)
 select
 	 ISNULL(MAX(dimDate.DateKey),-1) date_uid
-	,ISNULL(dimPerson.uid,-1) person_uid
+	,ISNULL(subpers.uid,ISNULL(pers.uid,-1)) person_uid
 	,ISNULL(dimIssueType.uid,-1) issuetype_uid
 	,10 bonustype_uid
-	,cfv.NUMBERVALUE bonus
+	,ISNULL(SUM(subcfv.NUMBERVALUE),ISNULL(SUM(cfv.NUMBERVALUE),0)) bonus
 	,ji.ID issueid
 from jiraissue ji
 	join changegroup cg on cg.issueid=ji.id
 	join changeitem ci on ci.groupid=cg.id and field='status'
 	join customfieldvalue cfv on cfv.ISSUE=ji.ID
+	
+	left outer join issuelink il on il.source=ji.ID and il.linktype=10000
+	left outer join jiraissue subji on il.DESTINATION=subji.ID
+	left outer join customfieldvalue subcfv on subcfv.ISSUE=subji.ID and subcfv.CUSTOMFIELD=10135
+	
 	left outer join dimDate on dimDate.FullDate=DATEADD(dd, 0, DATEDIFF(dd, 0, cg.created))
-	left outer join dimPerson on dimPerson.ADname=ji.assignee
+	left outer join dimPerson pers on pers.ADname=ji.assignee
+	left outer join dimPerson subpers on subpers.ADname=subji.assignee
 	left outer join dimIssueType on dimIssueType.issuetype_id=ji.issuetype and dimIssueType.project_id=ji.PROJECT
 where ji.project=10140 
 	and ji.issuetype=29 
 	and ji.resolution=1 
 	and newvalue='10009'
 	and cfv.CUSTOMFIELD=10135
-group by ji.id,dimIssueType.uid,dimPerson.uid,cfv.NUMBERVALUE;
+	
+group by ji.id,dimIssueType.uid,pers.uid,subpers.uid;
 
 SET NOCOUNT ON -- turn the annoying messages back on
 END
