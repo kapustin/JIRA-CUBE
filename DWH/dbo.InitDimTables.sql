b3e38277ff2bc45767dbbc3d47fe52a32110227d
Sergey <sergey.dvornikov@gmail.com>
2013-09-25 06:41:32 -0700

    Добавил элементы-заглушки в некоторые измерения

--- DWH/dbo.InitDimTables.sql
+++ DWH/dbo.InitDimTables.sql
@@ -14,8 +14,19 @@ INSERT INTO  dbo.dimIssueStatus (uid,name,description)
     VALUES (-1,'Не определено','Не определено');	
 INSERT INTO dbo.dimPerson (uid,ADName,ShortName,TabNum,FullName,Position,Department,Division)
     VALUES (-1,'Не определено','Не определено',-1,'Не определено','Не определено','Не определено','Не определено');
-INSERT INTO dbo.dimService (context,name)
-    VALUES ('Не определено','Не определено');
+    
+SET IDENTITY_INSERT dimService ON;
+INSERT INTO dbo.dimService (uid,context,name)
+    VALUES (-1,'Не определено','Не определено');
+SET IDENTITY_INSERT dimService OFF;
+
+SET IDENTITY_INSERT dimIssueType ON;
+INSERT INTO  dbo.dimIssueType (uid, issuetype_id, issuetype_name, issuetype_desc,project_id,project_key,project_name,project_desc) 
+    VALUES (-1,-1,'Не определено','Не определено',-1,'Не определено','Не определено','Не определено');
+SET IDENTITY_INSERT dimIssueType OFF;
+
+INSERT INTO  dbo.dimIssue (uid, pkey, info, client_uid, service_uid, priority_uid,issuetype_uid) 
+VALUES (-1,'N/A','Не определено',-1,-1,-1,-1);
     
 -- Наполнение измерения 'Тип бонуса'
 INSERT INTO dbo.dimBonusType (uid,name,description)
